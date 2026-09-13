e4ef54df64c1df2691ee6475128203e3fd4ba4f0
fqy.chun <2295860332@qq.com>
2026-09-13 21:30:58 +0800

    add some function

--- analyzer.py
+++ analyzer.py
@@ -8,3 +8,16 @@ def count_words(txt):
             result[word]=1
     return result
 
+def total_words(txt):
+    words = txt.replace(".", "").split()
+    return len(words)
+def unique_words(txt):
+    words = txt.replace(".", "").split()
+    return len(set(words))
+def Top3_words(txt):
+    words = count_words(txt)
+    result_list=list(words.items())
+
+    result_list.sort(key=lambda x:x[1],reverse=True)
+
+    return [result_list[0][0],result_list[1][0],result_list[2][0]]
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from file_reader import read_file
 from analyzer import count_words
+from analyzer import total_words
+from analyzer import unique_words
+from analyzer import Top3_words
 text=read_file("data/test.txt")
 # print(text)
 result=count_words(text)
 # result=sorted(result.items(),key=lambda x:x[1],reverse=True)
+print("====== Text Analysis Report ========================================")
 print(result)
+print("total words:",total_words(text))
+print("unique words:",unique_words(text))
+print("top3 words",Top3_words(text))
+print("====================================================================")
